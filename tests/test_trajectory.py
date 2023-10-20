d87b31613878723dea89412dc488a2827b0759aa
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2023-10-20 10:01:46 +0200

    Add trajectory restart for valid trajectories.

--- tests/test_trajectory.py
+++ tests/test_trajectory.py
@@ -5,8 +5,8 @@ from pytams.fmodel import ForwardModel
 from pytams.trajectory import Trajectory
 
 
-class DummyFModel(ForwardModel):
-    """Dummy child model.
+class SimpleFModel(ForwardModel):
+    """Simple forward model.
 
     The state is the time and score
     10 times the state, ceiled to 1.0
@@ -55,7 +55,7 @@ def test_initParametrizedTraj():
     assert t_test.stepSize() == 0.01
 
 
-def test_restartTraj():
+def test_restartEmptyTraj():
     """Test (empty) trajectory restart."""
     fmodel = {}
     parameters = {}
@@ -74,12 +74,12 @@ def test_templateModelExceptions():
     }
     t_test = Trajectory(fmodel, parameters)
     with pytest.raises(Exception):
-        t_test.advance(0.01)
+        t_test.advance()
 
 
-def test_dummyModelTraj():
-    """Test trajectory with dummy model."""
-    fmodel = DummyFModel()
+def test_simpleModelTraj():
+    """Test trajectory with simple model."""
+    fmodel = SimpleFModel()
     parameters = {
         "traj.end_time": 0.04,
         "traj.step_size": 0.001,
@@ -91,3 +91,17 @@ def test_dummyModelTraj():
     assert t_test.isConverged() is False
     t_test.advance()
     assert t_test.isConverged() is True
+
+
+def test_restartSimpleTraj():
+    """Test trajectory restart."""
+    fmodel = SimpleFModel()
+    parameters = {
+        "traj.end_time": 0.04,
+        "traj.step_size": 0.001,
+        "traj.targetScore": 0.25,
+    }
+    t_test = Trajectory(fmodel, parameters)
+    t_test.advance(0.01)
+    rst_test = Trajectory.restartFromTraj(t_test, 0.05)
+    assert rst_test.ctime() == 0.005
