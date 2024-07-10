622155e9e831197cdd064650e65b4fbc848488ea
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2024-07-10 12:04:31 +0200

    Update the base forward model to be an ABC. Child class only has to overwrite a few selected components.

--- pytams/fmodel.py
+++ pytams/fmodel.py
@@ -1,5 +1,6 @@
 from typing import Any
 from typing import Optional
+from abc import ABCMeta, abstractmethod
 
 
 class ForwardModelError(Exception):
@@ -8,28 +9,66 @@ class ForwardModelError(Exception):
     pass
 
 
-class TemplateCallError(ForwardModelError):
-    """Template ForwardModel method called !"""
+class BaseClassCallError(ForwardModelError):
+    """BaseClass ForwardModel method called !"""
 
     pass
 
 
-class ForwardModel:
-    """A template class for the stochastic forward model.
+class AdvanceError(ForwardModelError):
+    """Concrete ForwardModel _advance error !"""
+
+    pass
+
+
+class ForwardModelBaseClass(metaclass=ABCMeta):
+    """A base class for the stochastic forward model.
 
     Implement the core methods required of the forward
     model within the TAMS context. Exception are thrown
-    if those functions are not overritten in actual model.
+    if required functions are not overritten in actual model.
     """
+
 
     def __init__(self,
                  params: Optional[dict] = None,
                  ioprefix: Optional[str] = None):
-        """Might need something here."""
+        """Base class __init__ method.
+
+        The base forwardmodel class handles some components
+        needed by TAMS regardless of the model.
+
+        Args:
+            params: an optional dict containing parameters
+            ioprefix: an optional string defining run folder (TOCHECK)
+        """
+        self._init_model(params, ioprefix)
+        self._prescribed_noise : bool = False
+        self._noise : Any = None
+        self._step : int = 0
+
+    @abstractmethod
+    def _init_model(self,
+                    params: Optional[dict] = None,
+                    ioprefix: Optional[str] = None) -> None:
+        """Concrete class specific initialization.
+
+        Args:
+            params: an optional dict containing parameters
+            ioprefix: an optional string defining run folder (TOCHECK)
+        """
         pass
 
-    def advance(self, dt: float, forcingAmpl: float) -> float:
-        """Advance function of the model.
+
+    def advance(self,
+                dt: float,
+                forcingAmpl: float) -> float:
+        """Base class advance function of the model.
+
+        The base class advance function update the internal
+        step counter and manage the generation (or reuse)
+        of the stochastic noise.
+        It also handles exceptions.
 
         Args:
             dt: the time step size over which to advance
@@ -37,27 +76,63 @@ class ForwardModel:
         Return:
             Some model will not do exactly dt (e.g. sub-stepping) return the actual dt
         """
-        raise TemplateCallError("Calling advance() method !")
+        # Get noise for the next model step
+        if not self._prescribed_noise:
+            self._noise = self._get_noise()
+
+        try:
+            actual_dt = self._advance(self._step, dt, self._noise, forcingAmpl)
+        except:
+            raise AdvanceError("Damn it !")
+
+        self._prescribed_noise = False
+
+        return actual_dt
+
+    @abstractmethod
+    def _advance(self,
+                 step: int,
+                 dt: float,
+                 noise: Any,
+                 forcingAmpl: float) -> float:
+        """Concrete class advance function.
+
+        Args:
+            step: the current step counter
+            dt: the time step size over which to advance
+            noise: the noise to be used in the model step
+            forcingAmpl: stochastic multiplicator
+        Return:
+            Some model will not do exactly dt (e.g. sub-stepping) return the actual dt
+        """
+        raise BaseClassCallError("Calling base class _advance() method !")
+
 
     def getCurState(self) -> Any:
         """Return the current state of the model."""
-        raise TemplateCallError("Calling getCurState() method !")
+        raise BaseClassCallError("Calling getCurState() method !")
 
     def setCurState(self, state : Any) -> Any:
         """Set the current state of the model."""
-        raise TemplateCallError("Calling setCurState method !")
+        raise BaseClassCallError("Calling setCurState method !")
 
     def score(self) -> Any:
         """Return the model's current state score."""
-        raise TemplateCallError("Calling score method !")
+        raise BaseClassCallError("Calling score method !")
 
     def getNoise(self) -> Any:
         """Return the model's latest noise increment."""
-        raise TemplateCallError("Calling getNoise method !")
+        return self._noise
+
+    @abstractmethod
+    def _get_noise(self) -> Any:
+        """Return the model's latest noise increment."""
+        raise BaseClassCallError("Calling getNoise method !")
 
     def setNoise(self, a_noise : Any) -> None:
         """Set the model's next noise increment."""
-        raise TemplateCallError("Calling setNoise method !")
+        self._prescribed_noise = True
+        self._noise = a_noise
 
     def clear(self) -> None:
         """Destroy internal data."""
@@ -66,4 +141,4 @@ class ForwardModel:
     @classmethod
     def name(cls) -> str:
         """Return a the model name."""
-        return "TemplateForwardModel"
+        return "BaseClassForwardModel"
