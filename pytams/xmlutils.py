0dd224e6e0ada72a9a0c79562a905179d651c5c4
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2025-02-12 10:57:00 +0100

    Format long numpy array in xml

--- pytams/xmlutils.py
+++ pytams/xmlutils.py
@@ -5,11 +5,18 @@ from datetime import datetime
 from typing import Any
 from typing import Callable
 from typing import Dict
+from contextlib import contextmanager
 import numpy as np
 
 _logger = logging.getLogger(__name__)
 
-
+@contextmanager
+def oneliner_ndarray():
+    """Force ndarray print on a single line temporarily."""
+    oldoptions = np.get_printoptions()
+    np.set_printoptions(linewidth=np.inf)
+    yield
+    np.set_printoptions(**oldoptions)
 
 class XMLUtilsError(Exception):
     """Exception class for the xmlutils."""
@@ -175,7 +182,8 @@ def make_xml_snapshot(idx: int,
     elem.attrib["time"] = str(time)
     elem.attrib["score"] = str(score)
     elem.attrib["noise_type"] = get_val_type(noise)
-    elem.attrib["noise"] = str(noise)
+    with oneliner_ndarray():
+        elem.attrib["noise"] = str(noise)
     if state is None:
         elem.attrib["state_type"] = "None"
     else:
