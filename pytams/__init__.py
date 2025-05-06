08213fad331965b45d253a1e3b2dfb3f98be156b
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2025-05-06 12:08:45 +0200

    Bump to 0.0.5 before packaging.

--- pytams/__init__.py
+++ pytams/__init__.py
@@ -5,4 +5,4 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Lucas Esclapez"
 __email__ = "l.esclapez@esciencecenter.nl"
-__version__ = "0.0.4"
+__version__ = "0.0.5"
