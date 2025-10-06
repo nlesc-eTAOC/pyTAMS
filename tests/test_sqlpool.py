852ceaa9c5861a80a1ab26e3fe1f0355478ebdc7
Lucas Esclapez <lucas.esclapez@gmail.com>
2025-10-06 23:25:30 +0200

    Update tests.

--- tests/test_sqlpool.py
+++ tests/test_sqlpool.py
@@ -14,7 +14,7 @@ def test_createdb():
 def test_createdb_inmemory():
     """Initialize a SQLFile in memory."""
     poolfile = SQLFile("", in_memory=True)
-    assert poolfile.name() is None
+    assert poolfile.name() is ""
 
 def test_createdb_fail():
     """Fail to initialize a SQLFile."""
@@ -62,29 +62,29 @@ def test_add_traj_and_fetch():
 
 def test_fetch_unknown_traj():
     """Fetch an unknown trajectory."""
-    poolFile = SQLFile("test.db")
-    poolFile.add_trajectory("test.xml","")
-    assert poolFile.get_trajectory_count() == 1
+    poolfile = SQLFile("test.db")
+    poolfile.add_trajectory("test.xml","")
+    assert poolfile.get_trajectory_count() == 1
     with pytest.raises(ValueError):
-        _ = poolFile.fetch_trajectory(1)
+        _ = poolfile.fetch_trajectory(1)
     Path("./test.db").unlink(missing_ok=True)
 
-def test_archive_and_fetch_traj_to_DB():
+def test_archive_and_fetch_traj_to_db():
     """Archive a trajectory to SQLFile."""
-    poolFile = SQLFile("test.db")
-    poolFile.archive_trajectory("test.xml","")
-    assert poolFile.get_archived_trajectory_count() == 1
-    traj = poolFile.fetch_archived_trajectory(0)
+    poolfile = SQLFile("test.db")
+    poolfile.archive_trajectory("test.xml","")
+    assert poolfile.get_archived_trajectory_count() == 1
+    traj = poolfile.fetch_archived_trajectory(0)
     assert traj == "test.xml"
     Path("./test.db").unlink(missing_ok=True)
 
 def test_fetch_unknown_archived_traj():
     """Fetch an unknown archived trajectory."""
-    poolFile = SQLFile("test.db")
-    poolFile.archive_trajectory("test.xml","")
-    assert poolFile.get_archived_trajectory_count() == 1
+    poolfile = SQLFile("test.db")
+    poolfile.archive_trajectory("test.xml","")
+    assert poolfile.get_archived_trajectory_count() == 1
     with pytest.raises(ValueError):
-        _ = poolFile.fetch_archived_trajectory(1)
+        _ = poolfile.fetch_archived_trajectory(1)
     Path("./test.db").unlink(missing_ok=True)
 
 def test_lock_trajectory():
@@ -97,66 +97,77 @@ def test_lock_trajectory():
 
 def test_lock_locked_trajectory():
     """Lock an already locked trajectory in the SQLFile."""
-    poolFile = SQLFile("test.db")
-    poolFile.add_trajectory("test.xml","")
-    status = poolFile.lock_trajectory(0)
-    status = poolFile.lock_trajectory(0)
+    poolfile = SQLFile("test.db")
+    poolfile.add_trajectory("test.xml","")
+    status = poolfile.lock_trajectory(0)
+    status = poolfile.lock_trajectory(0)
     assert status is False
     Path("./test.db").unlink(missing_ok=True)
 
 def test_lock_and_release_trajectory():
     """Lock and release a trajectory in the SQLFile."""
-    poolFile = SQLFile("test.db")
-    poolFile.add_trajectory("test.xml","")
-    status = poolFile.lock_trajectory(0)
-    poolFile.release_trajectory(0)
-    status = poolFile.lock_trajectory(0)
+    poolfile = SQLFile("test.db")
+    poolfile.add_trajectory("test.xml","")
+    status = poolfile.lock_trajectory(0)
+    poolfile.release_trajectory(0)
+    status = poolfile.lock_trajectory(0)
     assert status
     Path("./test.db").unlink(missing_ok=True)
 
 def test_lock_and_release_multiple_trajectory():
     """Lock and release several trajectory in the SQLFile."""
-    poolFile = SQLFile("test.db")
+    poolfile = SQLFile("test.db")
     for i in range(10):
-        poolFile.add_trajectory(f"test{i}.xml","")
-        status = poolFile.lock_trajectory(0)
-    poolFile.release_all_trajectories()
+        poolfile.add_trajectory(f"test{i}.xml","")
+        status = poolfile.lock_trajectory(0)
+    poolfile.release_all_trajectories()
     status = True
     for _ in range(10):
-        status = status or poolFile.lock_trajectory(0)
+        status = status or poolfile.lock_trajectory(0)
     assert status
     Path("./test.db").unlink(missing_ok=True)
 
 def test_lock_unknown_trajectory():
     """Lock an unknown trajectory in the SQLFile."""
-    poolFile = SQLFile("test.db")
-    poolFile.add_trajectory("test.xml","")
+    poolfile = SQLFile("test.db")
+    poolfile.add_trajectory("test.xml","")
     with pytest.raises(ValueError):
-        _ = poolFile.lock_trajectory(1)
+        _ = poolfile.lock_trajectory(1)
     Path("./test.db").unlink(missing_ok=True)
 
-def test_lock_in_missing_DB():
+def test_lock_in_missing_db():
     """Lock a trajectory in a missing SQLFile."""
-    poolFile = SQLFile("test.db")
-    poolFile.add_trajectory("test.xml","")
+    poolfile = SQLFile("test.db")
+    poolfile.add_trajectory("test.xml","")
     Path("./test.db").unlink(missing_ok=True)
     with pytest.raises(SQLAlchemyError):
-        _ = poolFile.lock_trajectory(0)
+        _ = poolfile.lock_trajectory(0)
 
 def test_release_unknown_trajectory():
     """Release an unknown trajectory in the SQLFile."""
-    poolFile = SQLFile("test.db")
-    poolFile.add_trajectory("test.xml","")
+    poolfile = SQLFile("test.db")
+    poolfile.add_trajectory("test.xml","")
     with pytest.raises(ValueError):
-        poolFile.release_trajectory(1)
+        poolfile.release_trajectory(1)
     Path("./test.db").unlink(missing_ok=True)
 
 def test_splitting_data_add():
     """Adding splitting data to the database."""
     poolfile = SQLFile("test.db")
     for i in range(10):
-        poolfile.add_splitting_data(i, 1, 0.1, [0], [0], [0.0], [0.0, 0.0])
+        poolfile.add_splitting_data(i, 1, 0.1, [i-1], [0], [0.0], [0.0, 0.0])
+        poolfile.mark_last_iteration_as_completed()
     Path("./test.db").unlink(missing_ok=True)
+
+def test_splitting_data_add_and_query():
+    """Adding splitting data to the database."""
+    poolfile = SQLFile("", in_memory=True)
+    for i in range(10):
+        poolfile.add_splitting_data(i, 1, 0.1, [i-1], [0], [0.0], [0.0, 0.0])
+        poolfile.mark_last_iteration_as_completed()
+    assert poolfile.get_ongoing() is None
+    poolfile.add_splitting_data(10, 1, 0.1, [10-1,1,56], [0], [0.0], [0.0, 0.0])
+    assert poolfile.get_ongoing() == [9,1,56]
 
 def test_dump_json():
     """Dump the content of the DB to a json file."""
--- tests/test_tams.py
+++ tests/test_tams.py
@@ -1,5 +1,4 @@
 """Tests for the pytams.tams class."""
-import os
 import shutil
 from pathlib import Path
 import pytest
@@ -10,34 +9,34 @@ from tests.models import DoubleWellModel
 from tests.models import SimpleFModel
 
 
-def test_initTAMS():
+def test_init_tams():
     """Test TAMS initialization."""
     fmodel = SimpleFModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 500, "nsplititer": 200},
                    "trajectory" : {"end_time": 0.02, "step_size": 0.001}}, f)
     tams = TAMS(fmodel_t=fmodel, a_args=[])
     assert tams.n_traj() == 500
 
-def test_initTAMSMissingReq():
+def test_init_tams_missing_req():
     """Test failed TAMS initialization."""
     fmodel = SimpleFModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"nsplititer": 200},
                    "trajectory" : {"end_time": 0.02, "step_size": 0.001}}, f)
     with pytest.raises(ValueError):
         _ = TAMS(fmodel_t=fmodel, a_args=[])
 
-def test_initTAMSNoInput():
+def test_init_tams_no_input():
     """Test failed TAMS initialization."""
     fmodel = SimpleFModel
     with pytest.raises(ValueError):
         _ = TAMS(fmodel_t=fmodel, a_args=["-i", "dummy.toml"])
 
-def test_simpleModelTAMS():
+def test_simple_model_tams():
     """Test TAMS with simple model."""
     fmodel = SimpleFModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 100, "nsplititer": 200, "loglevel": "WARNING"},
                    "runner": {"type" : "asyncio"},
                    "trajectory": {"end_time": 0.02, "step_size": 0.001, "targetscore": 0.15}}, f)
@@ -45,10 +44,10 @@ def test_simpleModelTAMS():
     transition_proba = tams.compute_probability()
     assert transition_proba == 1.0
 
-def test_simpleModelTAMSwithDB():
+def test_simple_model_tams_with_db():
     """Test TAMS with simple model."""
     fmodel = SimpleFModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 100, "nsplititer": 200, "loglevel": "WARNING"},
                    "runner": {"type" : "dask"},
                    "database" : {"path" : "simpleModelTest.tdb"},
@@ -59,22 +58,22 @@ def test_simpleModelTAMSwithDB():
     assert transition_proba == 1.0
     shutil.rmtree("simpleModelTest.tdb")
 
-def test_simpleModelTAMSSlurmFail():
+def test_simple_model_tams_slurm_fail():
     """Test TAMS with simple model with Slurm dask backend."""
     fmodel = SimpleFModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 100, "nsplititer": 200, "loglevel": "DEBUG"},
                    "runner": {"type" : "dask"},
                    "dask": {"backend" : "slurm", "slurm_config_file": "dummy.yaml"},
                    "trajectory": {"end_time": 0.02, "step_size": 0.001, "targetscore": 0.15}}, f)
     tams = TAMS(fmodel_t=fmodel, a_args=[])
-    with pytest.raises(Exception):
+    with pytest.raises(FileNotFoundError):
         tams.compute_probability()
 
-def test_simpleModelTwiceTAMS():
+def test_simple_model_twice_tams():
     """Test TAMS with simple model."""
     fmodel = SimpleFModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 100, "nsplititer": 200, "loglevel": "WARNING",
                             "logfile": "test.log"},
                    "runner": {"type" : "asyncio"},
@@ -88,29 +87,29 @@ def test_simpleModelTwiceTAMS():
     tams = TAMS(fmodel_t=fmodel, a_args=[])
     transition_proba = tams.compute_probability()
     ndb = 0
-    for folder in os.listdir("."):
+    for folder in Path("./").iterdir():
         if "simpleModelTest" in str(folder):
             shutil.rmtree(folder)
             ndb += 1
     assert ndb == 2
-    assert os.path.exists("test.log")
-    os.remove("test.log")
+    assert Path("test.log").exists()
+    Path("test.log").unlink(missing_ok=True)
 
-def test_stallingSimpleModelTAMS():
+def test_stalling_simplemodel_tams():
     """Test TAMS with simple model and stalled score function."""
     fmodel = SimpleFModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 100, "nsplititer": 200, "loglevel": "ERROR"},
                    "runner": {"type" : "asyncio"},
                    "trajectory": {"end_time": 1.0, "step_size": 0.01, "targetscore": 1.1}}, f)
     tams = TAMS(fmodel_t=fmodel, a_args=[])
-    with pytest.raises(Exception):
+    with pytest.raises(RuntimeError):
       tams.compute_probability()
 
-def test_doublewellModelTAMS():
+def test_doublewell_tams():
     """Test TAMS with the doublewell model."""
     fmodel = DoubleWellModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 50, "nsplititer": 200, "walltime": 500.0},
                    "runner": {"type" : "dask"},
                    "model": {"noise_amplitude" : 0.8},
@@ -120,10 +119,10 @@ def test_doublewellModelTAMS():
     transition_proba = tams.compute_probability()
     assert transition_proba >= 0.2
 
-def test_doublewellModelSaveTAMS():
+def test_doublewell_save_tams():
     """Test TAMS with the doublewell model."""
     fmodel = DoubleWellModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 50, "nsplititer": 100, "walltime": 500.0},
                    "runner": {"type" : "dask"},
                    "database": {"path": "dwTest.tdb"},
@@ -133,13 +132,13 @@ def test_doublewellModelSaveTAMS():
     tams = TAMS(fmodel_t=fmodel, a_args=[])
     transition_proba = tams.compute_probability()
     assert transition_proba >= 0.2
-    os.remove("input.toml")
+    Path("input.toml").unlink(missing_ok=True)
     shutil.rmtree("dwTest.tdb")
 
-def test_doublewellDeterministicModelTAMS():
+def test_doublewell_deterministic_tams():
     """Test TAMS with the doublewell model."""
     fmodel = DoubleWellModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 100, "nsplititer": 400,
                             "walltime": 500.0, "deterministic": True},
                    "runner": {"type" : "asyncio"},
@@ -149,13 +148,13 @@ def test_doublewellDeterministicModelTAMS():
     tams = TAMS(fmodel_t=fmodel, a_args=[])
     transition_proba = tams.compute_probability()
     assert transition_proba == 0.46117490875393713
-    os.remove("input.toml")
+    Path("input.toml").unlink(missing_ok=True)
 
-@pytest.mark.dependency()
-def test_doublewellModel2WorkersTAMS():
+@pytest.mark.dependency
+def test_doublewell_2_workers_tams():
     """Test TAMS with the doublewell model using two workers."""
     fmodel = DoubleWellModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 50, "nsplititer": 400,
                             "walltime": 500.0, "deterministic": True},
                    "runner": {"type" : "dask", "nworker_init": 2, "nworker_iter": 2},
@@ -165,22 +164,22 @@ def test_doublewellModel2WorkersTAMS():
                                   "targetscore": 0.5}}, f)
     tams = TAMS(fmodel_t=fmodel, a_args=[])
     transition_proba = tams.compute_probability()
-    assert transition_proba == 0.6382393305518409
-    os.remove("input.toml")
+    assert transition_proba == 0.6254745180342447
+    Path("input.toml").unlink(missing_ok=True)
 
-@pytest.mark.dependency(depends=["test_doublewellModel2WorkersTAMS"])
-def test_doublewellModel2WorkersLoadDB():
+@pytest.mark.dependency(depends=["test_doublewell_2_workers_tams"])
+def test_doublewell_2_workers_load_db():
     """Load the database from previous test."""
     tdb = Database.load(Path("dwTest.tdb"))
     tdb.load_data(True)
     assert tdb.traj_list_len() == 50
     assert tdb.archived_traj_list_len() == 22
 
-@pytest.mark.dependency(depends=["test_doublewellModel2WorkersTAMS"])
-def test_doublewellModel2WorkersRestoreTAMS():
+@pytest.mark.dependency(depends=["test_doublewell_2_workers_tams"])
+def test_doublewell_2_workers_restore_tams():
     """Test TAMS with the doublewell model using two workers and restoring."""
     fmodel = DoubleWellModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 100, "nsplititer": 400, "walltime": 500.0},
                    "database": {"path": "dwTest.tdb"},
                    "runner": {"type" : "asyncio", "nworker_init": 2, "nworker_iter": 2},
@@ -190,16 +189,15 @@ def test_doublewellModel2WorkersRestoreTAMS():
     tams = TAMS(fmodel_t=fmodel, a_args=[])
     transition_proba = tams.compute_probability()
     assert transition_proba >= 0.2
-    os.remove("input.toml")
+    Path("input.toml").unlink(missing_ok=True)
     shutil.rmtree("dwTest.tdb")
 
-
-def test_doublewellVerySlowTAMS():
+def test_doublewell_very_slow_tams():
     """Test TAMS run out of time with a slow doublewell."""
     fmodel = DoubleWellModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 10, "nsplititer": 400, "walltime": 3.0},
-                   "database": {"path": "dwTest.tdb"},
+                   "database": {"path": "vslowdwTest.tdb"},
                    "runner": {"type" : "dask", "nworker_init": 1, "nworker_iter": 1},
                    "trajectory": {"end_time": 10.0, "step_size": 0.01,
                                   "targetscore": 0.7},
@@ -208,16 +206,16 @@ def test_doublewellVerySlowTAMS():
     tams = TAMS(fmodel_t=fmodel, a_args=[])
     transition_proba = tams.compute_probability()
     assert transition_proba <= 0.0
-    os.remove("input.toml")
-    shutil.rmtree("dwTest.tdb")
+    Path("input.toml").unlink(missing_ok=True)
+    shutil.rmtree("vslowdwTest.tdb")
 
-@pytest.mark.dependency()
-def test_doublewellSlowTAMS():
+@pytest.mark.dependency
+def test_doublewell_slow_tams():
     """Test TAMS run out of time with a slow doublewell."""
     fmodel = DoubleWellModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 10, "nsplititer": 400, "walltime": 3.0},
-                   "database": {"path": "dwTest.tdb"},
+                   "database": {"path": "slowdwTest.tdb"},
                    "runner": {"type" : "asyncio", "nworker_init": 1, "nworker_iter": 1},
                    "trajectory": {"end_time": 10.0, "step_size": 0.01,
                                   "targetscore": 0.7},
@@ -226,15 +224,15 @@ def test_doublewellSlowTAMS():
     tams = TAMS(fmodel_t=fmodel, a_args=[])
     transition_proba = tams.compute_probability()
     assert transition_proba <= 0.0
-    os.remove("input.toml")
+    Path("input.toml").unlink(missing_ok=True)
 
-@pytest.mark.dependency(depends=["test_doublewellSlowTAMS"])
-def test_doublewellSlowRestoreTAMS():
+@pytest.mark.dependency(depends=["test_doublewell_slow_tams"])
+def test_doublewell_slow_restore_tams():
     """Test TAMS restarting a slow doublewell."""
     fmodel = DoubleWellModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 10, "nsplititer": 400, "walltime": 3.0},
-                   "database": {"path": "dwTest.tdb"},
+                   "database": {"path": "slowdwTest.tdb"},
                    "runner": {"type" : "asyncio", "nworker_init": 1, "nworker_iter": 1},
                    "trajectory": {"end_time": 10.0, "step_size": 0.01,
                                   "targetscore": 0.7},
@@ -243,13 +241,13 @@ def test_doublewellSlowRestoreTAMS():
     tams = TAMS(fmodel_t=fmodel, a_args=[])
     transition_proba = tams.compute_probability()
     assert transition_proba <= 0.0
-    os.remove("input.toml")
-    shutil.rmtree("dwTest.tdb")
+    Path("input.toml").unlink(missing_ok=True)
+    shutil.rmtree("slowdwTest.tdb")
 
-def test_doublewellSlowTAMSRestoreMoreSplit():
+def test_doublewell_slow_tams_restore_more_split():
     """Test restart TAMS more splitting iterations."""
     fmodel = DoubleWellModel
-    with open("input.toml", 'w') as f:
+    with Path("input.toml").open("w") as f:
         toml.dump({"tams": {"ntrajectories": 20, "nsplititer": 20,
                             "walltime": 10.0, "deterministic": True},
                    "database": {"path": "dwTest.tdb"},
@@ -265,5 +263,5 @@ def test_doublewellSlowTAMSRestoreMoreSplit():
     tams_load._tdb._nsplititer = 30
     transition_proba = tams_load.compute_probability()
     assert transition_proba == 0.04244060468762377
-    os.remove("input.toml")
+    Path("input.toml").unlink(missing_ok=True)
     shutil.rmtree("dwTest.tdb")
