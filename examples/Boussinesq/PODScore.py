3a9f1a70044f9fb45c0152707856cf191c3a862d
Lucas Esclapez <lucas.esclapez@gmail.com>
2025-10-06 09:57:19 +0200

    Update the score function to account for changed in POD data.

--- examples/Boussinesq/PODScore.py
+++ examples/Boussinesq/PODScore.py
@@ -1,108 +1,74 @@
 """A container class for POD-based score function."""
+
 import logging
-import numpy as np
-import netCDF4 as nc
 from pathlib import Path
+import netCDF4
+import numpy as np
+import numpy.typing as npt
 
 _logger = logging.getLogger(__name__)
 
-def project_in_POD_space(field, phi_POD, weights):
-    """ Computes POD coefficients of a snapshot
-    Parameters
-    ----------
-        field : np array
-            Snapshot to project [nfield,depth,lat]
-        phi_POD : np array
-            POD basis functions [nmodes,nfield,depth,lat]
-        weights : np array
-            Spatial weights for projection [depth,lat]
-    Returns
-    -------
-        field_POD : np array
+
+def project_in_pod_space(
+    nmodes: int, field: npt.NDArray[np.number], phi_pod: npt.NDArray[np.number], weights: npt.NDArray[np.number]
+) -> npt.NDArray[np.number]:
+    """Computes POD coefficients of a snapshot.
+
+    Args:
+        nmodes: Number of modes to project on to
+        field : Snapshot to project [nfield,depth,lat]
+        phi_pod : POD basis functions [nmodes,nfield,depth,lat]
+        weights : Spatial weights for projection [depth,lat]
+
+    Returns:
+        field_pod : np array
             POd coefficients of snapshot field [nmodes]
     """
+    field_pod = np.zeros(nmodes)
+    field_pod[:] = np.sum(field[None, :, :, :] * phi_pod[:nmodes, :, :, :] * weights[None, None, :, :], axis=(1, 2, 3))
 
-    nmodes = phi_POD.shape[0]
+    return field_pod
 
-    # Take weighed inner product to get projection coefficients onto POD basis
-    field_POD = np.zeros(nmodes)
-    for i in range(nmodes):
-        field_POD[i] = np.sum(field[:,:,:]*phi_POD[i,:,:,:]*weights[None,:,:])
 
-    # Return POD coefficients
-    return field_POD
+def compute_score_function(
+    field_pod: npt.NDArray[np.number], ref_psi_pod: npt.NDArray[np.number], curv_abs: npt.NDArray[np.number], d0: float
+) -> float:
+    """Computes score function using curvilinear abs.
 
-def compute_score_function(field_POD, ref_psi_POD):
-    """ Computes score function
-    Parameters
-    ----------
-        field_POD: np array
-            POD coefficients of field snapshot [nmodes]
-        ref_psi_POD: np array
-            Reference trajectory POD coefficients [ntime, nmodes]
-    Returns
-    -------
+    Args:
+        field_pod: POD coefficients of field snapshot [nmodes]
+        ref_psi_pod: Reference trajectory POD coefficients [ntime, nmodes]
+        curv_abs: The curvilinear abcissa along the reference traj [ntime]
+        d0: The exponential decay distance parameter
+
+    Returns:
         score: float
             score value
     """
-
     # Get length of trajectory data
-    ntime = ref_psi_POD.shape[0]
+    ntime = ref_psi_pod.shape[0]
 
     # Computes the Euclidian distance in POD space
     # between snapshot and trajectory points
     dist = np.zeros(ntime)
-    for t in range(ntime):
-        dist[t] = np.sum((field_POD[:]-ref_psi_POD[t,:])**2)
-
-    # Get closest index
-    it = np.argmin(dist)
-    # Computes penalty coefficient
-    alpha = 1.0 # function of dist[it] in the future
-
-    score = (it+1)/float(ntime)*alpha
-    return score
-
-def compute_score_function_curv_abs(field_POD, ref_psi_POD, curv_abs):
-    """ Computes score function using curvilinear abs
-    Parameters
-    ----------
-        field_POD: np array
-            POD coefficients of field snapshot [nmodes]
-        ref_psi_POD: np array
-            Reference trajectory POD coefficients [ntime, nmodes]
-        curv_abs: np array
-            The curvilinear abcissa along the reference traj [ntime]
-    Returns
-    -------
-        score: float
-            score value
-    """
-
-    # Get length of trajectory data
-    ntime = ref_psi_POD.shape[0]
-
-    # Computes the Euclidian distance in POD space
-    # between snapshot and trajectory points
-    dist = np.zeros(ntime)
-    for t in range(ntime):
-        dist[t] = np.sum((field_POD[:]-ref_psi_POD[t,:])**2)
-        dist[t] = np.sqrt(dist[t])
+    dist[:] = np.sqrt(np.sum((field_pod[None, :] - ref_psi_pod[:, :]) ** 2, axis=1))
 
     # Get closest index
     it = np.argmin(dist)
 
-    if (it > 0 and it < len(dist)-1):
+    if it > 0 and it < len(dist) - 1:
         # Get next adjacent index
-        it_next = it+1 if dist[it+1] <= dist[it-1] else it-1
+        it_next = it + 1 if dist[it + 1] <= dist[it - 1] else it - 1
 
         # dot product
-        dot = np.sum((field_POD[:]-ref_psi_POD[it,:]) * (ref_psi_POD[it_next,:] - ref_psi_POD[it,:]))
-        segment_norm = np.sum((ref_psi_POD[it_next,:] - ref_psi_POD[it,:]) * (ref_psi_POD[it_next,:] - ref_psi_POD[it,:]))
+        dot = np.sum((field_pod[:] - ref_psi_pod[it, :]) * (ref_psi_pod[it_next, :] - ref_psi_pod[it, :]))
+        segment_norm = np.sum(
+            (ref_psi_pod[it_next, :] - ref_psi_pod[it, :]) * (ref_psi_pod[it_next, :] - ref_psi_pod[it, :])
+        )
 
-        frac = dot/segment_norm if np.abs(dot/segment_norm) <= 1.0 else 0.0
-        closest_point = ref_psi_POD[it,:] + frac * (ref_psi_POD[it_next,:] - ref_psi_POD[it,:])
-        distance = np.sum((field_POD[:]-closest_point[:])**2)
+        frac = dot / segment_norm if np.abs(dot / segment_norm) <= 1.0 else 0.0
+        closest_point = ref_psi_pod[it, :] + frac * (ref_psi_pod[it_next, :] - ref_psi_pod[it, :])
+        distance = np.sum((field_pod[:] - closest_point[:]) ** 2)
         distance = np.sqrt(distance)
         curv = curv_abs[it] + frac * (curv_abs[it_next] - curv_abs[it])
 
@@ -111,31 +77,29 @@ def compute_score_function_curv_abs(field_POD, ref_psi_POD, curv_abs):
         curv = curv_abs[it]
 
     # Computes penalty coefficient
-    alpha = 1.0 * np.exp(-distance**2/1.0) # function of dist[it] in the future
+    alpha = 1.0 * np.exp(-(distance**2) / d0**2)  # function of dist[it] in the future
 
-    score = curv*alpha
-    return score
+    return curv * alpha
 
 
-class PODScore():
+class PODScore:
     """A class to hold POD data required for evaluating the score function."""
-    def __init__(self,
-                 lat_in: int,
-                 depth_in: int,
-                 pod_data_file: str) -> None:
+
+    def __init__(
+        self, lat_in: int, depth_in: int, pod_data_file: str, score_space_dim: int = 8, score_d0: float = 1.0
+    ) -> None:
         """Load the POD data and perform some checks.
 
         Args:
             lat_in: dimension in the latitude space
             depth_in: dimension in the depth space
             pod_data_file: the data containing the results of the POD decomposition
+            score_space_dim: the dimension of the low-dim space to get the score
+            score_d0: the decay distance from the original data traj
         """
-        # User defined parameters
-        self._nmodes = 8
-        self._ntraj_ref_start = 3200
-        self._ntraj_ref_end = 3800
-        self._ntraj_start = 0
-        self._ntraj_end = 119
+        # User-defined parameters
+        self._n_active_modes = score_space_dim
+        self._d0 = score_d0
 
         # Read POD decomposition data
         if not Path(pod_data_file).exists():
@@ -144,45 +108,61 @@ class PODScore():
             raise ValueError(err_msg)
 
         # Open data file and metadata
-        nc_POD_in = nc.Dataset(pod_data_file, mode="r")
-        shapes = nc_POD_in["stream_phi"].shape
-        self._ntimes = shapes[0]
-        self._lat = shapes[1]
-        self._depth = shapes[2]
+        nc_pod_in = netCDF4.Dataset(pod_data_file, mode="r")
+        self._ntimes = nc_pod_in.dimensions["time"].size
+        self._lat = nc_pod_in.dimensions["lat"].size
+        self._depth = nc_pod_in.dimensions["depth"].size
+        self._nmodes = nc_pod_in.dimensions["mode"].size
 
         # Checks
         if not (self._lat == lat_in and self._depth == depth_in):
-            err_msg = f"Model size does not match the POD database"
+            err_msg = "Model size does not match the POD database"
+            _logger.exception(err_msg)
+            raise ValueError(err_msg)
+
+        if self._n_active_modes > self._nmodes:
+            err_msg = f"Requested {self._n_active_modes} but only {self._nmodes} available !"
             _logger.exception(err_msg)
             raise ValueError(err_msg)
 
         # Load data
-        self._psi_POD = nc_POD_in["psi"][:,self._ntimes-self._nmodes:self._ntimes]
-        self._sigma_POD = nc_POD_in["sigma"][self._ntimes-self._nmodes:self._ntimes]
-        self._psi_POD[:,:] *= np.sqrt(self._sigma_POD[None,:])
-        self._phi_POD = np.zeros((self._nmodes,2,self._lat,self._depth))
-        self._phi_POD[:,0,:,:] = nc_POD_in["stream_phi"][self._ntimes-self._nmodes:self._ntimes,:,:]
-        self._phi_POD[:,1,:,:] = nc_POD_in["salt_phi"][self._ntimes-self._nmodes:self._ntimes,:,:]
-        self._phi_POD[:,:,:,:] /= np.sqrt(self._sigma_POD[:,None,None,None])
-        self._weights = nc_POD_in["spatial_weights"][:,:]
-        self._scaling_stream = nc_POD_in["scaling_stream"][:]
-        self._scaling_salt = nc_POD_in["scaling_salt"][:]
-        self._curv_abs = nc_POD_in["curv_abs"][:]
+        self._psi_pod = nc_pod_in["psi"][:, :]
+        self._sigma_pod = nc_pod_in["sigma"][:]
+        self._psi_pod[:, :] *= np.sqrt(self._sigma_pod[None, :])
+        self._phi_pod = np.zeros((self._nmodes, 2, self._lat, self._depth))
+        self._phi_pod[:, 0, :, :] = nc_pod_in["stream_phi"][:, :, :]
+        self._phi_pod[:, 1, :, :] = nc_pod_in["salt_phi"][:, :, :]
+        self._phi_pod[:, :, :, :] /= np.sqrt(self._sigma_pod[:, None, None, None])
+        self._weights = nc_pod_in["spatial_weights"][:, :]
+        self._scaling_stream = nc_pod_in["scaling_stream"][:]
+        self._scaling_salt = nc_pod_in["scaling_salt"][:]
 
-    def get_score(self, model_state) -> float:
+        # Compute the abscissa
+        self.compute_curv_abs()
+
+    def compute_curv_abs(self) -> None:
+        """Compute the curvilinear abscissa for getting the score."""
+        self._curv_abs = np.zeros(self._ntimes)
+        for k in range(1, self._ntimes):
+            self._curv_abs[k] = self._curv_abs[k - 1] + np.sqrt(
+                np.sum((self._psi_pod[k, : self._n_active_modes] - self._psi_pod[k - 1, : self._n_active_modes]) ** 2)
+            )
+
+        self._curv_abs = self._curv_abs / self._curv_abs[-1]
+
+    def get_score(self, model_state: npt.NDArray[np.number]) -> float:
         """Compute the score function of the given model state.
 
         Args:
-            The model state as a numpy array (fix typing)
+            model_state: The model state as a numpy array (fix typing)
 
         Returns:
             The score function associated with the input state
         """
-        field = np.zeros((2,self._lat,self._depth))
-        field[0,:,:] = model_state[3,:,:] / self._scaling_stream
-        field[1,:,:] = model_state[1,:,:] / self._scaling_salt
+        field = np.zeros((2, self._lat, self._depth))
+        field[0, :, :] = model_state[3, :, :] / self._scaling_stream
+        field[1, :, :] = model_state[1, :, :] / self._scaling_salt
 
-        field_POD = project_in_POD_space(field, self._phi_POD, self._weights)
+        field_pod = project_in_pod_space(self._n_active_modes, field, self._phi_pod, self._weights)
 
-        #return compute_score_function(field_POD, self._psi_POD[self._ntraj_ref_start:self._ntraj_ref_end,:])
-        return compute_score_function_curv_abs(field_POD, self._psi_POD[self._ntraj_ref_start:self._ntraj_ref_end,:], self._curv_abs)
+        return compute_score_function(field_pod, self._psi_pod[:, : self._n_active_modes], self._curv_abs, self._d0)
