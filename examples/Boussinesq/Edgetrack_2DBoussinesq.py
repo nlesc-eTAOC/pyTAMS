498e52b5ef5022c7f03ba9cb0d5f16d6a1d08c1c
Lucas Esclapez <lucas.esclapez@gmail.com>
2025-10-09 17:42:19 +0200

    Add ON-OFF state and fmodel to top level edge tracking function.

--- examples/Boussinesq/Edgetrack_2DBoussinesq.py
+++ examples/Boussinesq/Edgetrack_2DBoussinesq.py
@@ -2,6 +2,7 @@ import logging
 from pathlib import Path
 import numpy as np
 import toml
+import shutil
 from TAMS_2DBoussinesq import Boussinesq2DModel
 from pytams.trajectory import Trajectory
 from edgetracking_algorithm import edgetracking
@@ -13,15 +14,18 @@ if __name__ == "__main__":
     with open("input_edge.toml", "r") as f:
         input_params = toml.load(f)
 
-    traj = Trajectory(0, fmodel, input_params)
-    
-    on_state = ()   # ON state array
-    off_state = ()  # OFF state array
+    on_state = np.load("stateON_beta_0p1.npy", allow_pickle=True)
+    off_state = np.load("stateOFF_beta_0p1.npy", allow_pickle=True)
 
-    upper, lower, edgetrack = edgetracking(on_state, off_state,
+    # Create a temporary folder for the model
+    Path("./.edge_tmp/").mkdir()
+
+    upper, lower, edgetrack = edgetracking(fmodel, input_params, on_state, off_state,
         eps1 = 1e-3,
         eps2 = 5e-3,
         maxiter = 10
     )
+
+    shutil.rmtree(f"./.edge_tmp")
 
     # Save upper, lower, edgetrack as xr.DataArrays
