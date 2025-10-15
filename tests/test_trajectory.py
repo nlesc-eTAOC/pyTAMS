c3d6504b63f3695a3fae791a3cf32070b5423cc8
Lucas Esclapez <lucas.esclapez@gmail.com>
2025-10-15 15:04:17 +0200

    Lint and update trajectory tests.

--- tests/test_trajectory.py
+++ tests/test_trajectory.py
@@ -1,4 +1,5 @@
 """Tests for the pytams.trajectory class."""
+
 from math import isclose
 from pathlib import Path
 import pytest
@@ -6,36 +7,43 @@ from pytams.fmodel import ForwardModelBaseClass
 from pytams.trajectory import Snapshot
 from pytams.trajectory import Trajectory
 from pytams.utils import moving_avg
-from tests.models import SimpleFModel
 from tests.models import DoubleWellModel
+from tests.models import SimpleFModel
 
 
-def test_initSnapshot():
+def test_init_snapshot():
     """Test initialization of a snapshot."""
-    snap = Snapshot(0.1,0.1,"Noisy","State")
+    snap = Snapshot(0.1, 0.1, "Noisy", "State")
     assert snap.time == 0.1
     assert snap.has_state()
 
 
-def test_initSnapshotNoState():
+def test_init_snapshot_nostate():
     """Test initialization of a stateless snapshot."""
-    snap = Snapshot(0.1,0.1,"Noisy")
+    snap = Snapshot(0.1, 0.1, "Noisy")
     assert not snap.has_state()
 
 
-def test_initBaseClassError():
-    """Test using base class fmodel during trajectory creation."""
+def test_init_missing_basic_inputs():
+    """Test lack of minimal traj data in trajectory creation."""
     fmodel = ForwardModelBaseClass
     parameters = {}
-    with pytest.raises(Exception):
+    with pytest.raises(ValueError):
         _ = Trajectory(1, fmodel, parameters)
 
 
-def test_initBlankTraj():
+def test_init_baseclasserror():
+    """Test using base class fmodel during trajectory creation."""
+    fmodel = ForwardModelBaseClass
+    parameters = {"trajectory": {"end_time": 2.0, "step_size": 0.01}}
+    with pytest.raises(TypeError):
+        _ = Trajectory(1, fmodel, parameters)
+
+
+def test_init_blank_traj():
     """Test blank trajectory creation."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 2.0,
-                                  "step_size": 0.01}}
+    parameters = {"trajectory": {"end_time": 2.0, "step_size": 0.01}}
     t_test = Trajectory(1, fmodel, parameters)
     assert t_test.id() == 1
     assert t_test.idstr() == "traj000001_0000"
@@ -43,34 +51,29 @@ def test_initBlankTraj():
     assert t_test.score_max() == 0.0
 
 
-def test_initParametrizedTraj():
+def test_init_parametrized_traj():
     """Test parametrized trajectory creation."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 2.0,
-                                  "step_size": 0.01,
-                                  "targetscore": 0.25}}
+    parameters = {"trajectory": {"end_time": 2.0, "step_size": 0.01, "targetscore": 0.25}}
     t_test = Trajectory(1, fmodel, parameters)
-    t_test.set_workdir(Path("."))
+    t_test.set_workdir(Path())
     assert t_test.step_size() == 0.01
 
 
-def test_restartEmptyTraj():
+def test_restart_empty_traj():
     """Test (empty) trajectory restart."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 2.0,
-                                  "step_size": 0.01}}
-    fromTraj = Trajectory(1, fmodel, parameters)
-    rstTraj = Trajectory(2, fmodel, parameters)
-    rst_test = Trajectory.branch_from_trajectory(fromTraj, rstTraj, 0.1)
+    parameters = {"trajectory": {"end_time": 2.0, "step_size": 0.01}}
+    from_traj = Trajectory(1, fmodel, parameters)
+    rst_traj = Trajectory(2, fmodel, parameters)
+    rst_test = Trajectory.branch_from_trajectory(from_traj, rst_traj, 0.1)
     assert rst_test.current_time() == 0.0
 
 
-def test_simpleModelTraj():
+def test_simple_model_traj():
     """Test trajectory with simple model."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 0.04,
-                                  "step_size": 0.001,
-                                  "targetscore": 0.25}}
+    parameters = {"trajectory": {"end_time": 0.04, "step_size": 0.001, "targetscore": 0.25}}
     t_test = Trajectory(1, fmodel, parameters)
     t_test.advance(0.01)
     assert isclose(t_test.score_max(), 0.1, abs_tol=1e-9)
@@ -78,12 +81,11 @@ def test_simpleModelTraj():
     t_test.advance()
     assert t_test.is_converged() is True
 
-def test_branchSimpleModelTraj():
+
+def test_branch_simple_model_traj():
     """Test branching a trajectory with simple model."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 0.04,
-                                  "step_size": 0.0002,
-                                  "targetscore": 0.45}}
+    parameters = {"trajectory": {"end_time": 0.04, "step_size": 0.0002, "targetscore": 0.45}}
     t_ancestor = Trajectory(1, fmodel, parameters)
     t_ancestor.advance()
     assert t_ancestor.get_computed_steps_count() == 201
@@ -93,79 +95,71 @@ def test_branchSimpleModelTraj():
     t_branched.advance()
     assert t_branched.get_computed_steps_count() == 150
 
-def test_storeAndRestoreSimpleTraj():
+
+def test_store_and_restore_simple_traj():
     """Test store and restoring trajectory with simple model."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 0.05,
-                                  "step_size": 0.001,
-                                  "targetscore": 0.25}}
+    parameters = {"trajectory": {"end_time": 0.05, "step_size": 0.001, "targetscore": 0.25}}
     t_test = Trajectory(1, fmodel, parameters)
     t_test.advance(0.02)
     assert isclose(t_test.score_max(), 0.2, abs_tol=1e-9)
     assert t_test.is_converged() is False
-    chkFile = Path("./test.xml")
-    t_test.store(chkFile)
-    assert chkFile.exists() is True
-    rst_test = Trajectory.restore_from_checkfile(chkFile, fmodel, parameters)
+    chkfile = Path("./test.xml")
+    t_test.store(chkfile)
+    assert chkfile.exists() is True
+    rst_test = Trajectory.restore_from_checkfile(chkfile, fmodel, parameters)
     assert isclose(rst_test.score_max(), 0.2, abs_tol=1e-9)
     rst_test.advance()
     assert rst_test.is_converged() is True
-    chkFile.unlink()
+    chkfile.unlink(missing_ok=True)
 
 
-def test_storeAndRestoreFrozenSimpleTraj():
+def test_store_and_restore_frozen_simple_traj():
     """Test store and restoring frozen trajectory with simple model."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 0.05,
-                                  "step_size": 0.001,
-                                  "targetscore": 0.25}}
+    parameters = {"trajectory": {"end_time": 0.05, "step_size": 0.001, "targetscore": 0.25}}
     t_test = Trajectory(1, fmodel, parameters)
     t_test.advance(0.02)
     assert isclose(t_test.score_max(), 0.2, abs_tol=1e-9)
     assert t_test.is_converged() is False
-    chkFile = Path("./test.xml")
-    t_test.store(chkFile)
-    assert chkFile.exists() is True
-    rst_test = Trajectory.restore_from_checkfile(chkFile, fmodel, parameters, frozen=True)
+    chkfile = Path("./test.xml")
+    t_test.store(chkfile)
+    assert chkfile.exists() is True
+    rst_test = Trajectory.restore_from_checkfile(chkfile, fmodel, parameters, frozen=True)
     assert isclose(rst_test.score_max(), 0.2, abs_tol=1e-9)
-    with pytest.raises(Exception):
+    with pytest.raises(RuntimeError):
         rst_test.advance()
-    with pytest.raises(Exception):
+    with pytest.raises(RuntimeError):
         rst_test._one_step()
+    chkfile.unlink(missing_ok=True)
 
 
-def test_restartSimpleTraj():
+def test_restart_simple_traj():
     """Test trajectory restart."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 0.04,
-                                  "step_size": 0.001,
-                                  "targetscore": 0.25}}
-    fromTraj = Trajectory(1, fmodel, parameters)
-    fromTraj.advance(0.01)
-    rstTraj = Trajectory(2, fmodel, parameters)
-    rst_test = Trajectory.branch_from_trajectory(fromTraj, rstTraj, 0.05)
+    parameters = {"trajectory": {"end_time": 0.04, "step_size": 0.001, "targetscore": 0.25}}
+    from_traj = Trajectory(1, fmodel, parameters)
+    from_traj.advance(0.01)
+    rst_traj = Trajectory(2, fmodel, parameters)
+    rst_test = Trajectory.branch_from_trajectory(from_traj, rst_traj, 0.05)
     assert rst_test.current_time() == 0.006
 
 
-def test_accessDataSimpleTraj():
+def test_access_data_simple_traj():
     """Test trajectory data access."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 0.04,
-                                  "step_size": 0.001,
-                                  "targetscore": 0.25}}
+    parameters = {"trajectory": {"end_time": 0.04, "step_size": 0.001, "targetscore": 0.25}}
     t_test = Trajectory(1, fmodel, parameters)
     t_test.advance(0.01)
     assert t_test.get_length() == 11
     assert isclose(t_test.get_time_array()[-1], 0.01, abs_tol=1e-9)
     assert isclose(t_test.get_score_array()[-1], 0.1, abs_tol=1e-9)
 
-def test_sparseSimpleTraj():
+
+def test_sparse_simple_traj():
     """Test a sparse trajectory with simple model."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 0.04,
-                                  "step_size": 0.001,
-                                  "targetscore": 0.25,
-                                  "sparse_freq": 5}}
+    parameters = {"trajectory": {"end_time": 0.04, "step_size": 0.001, "targetscore": 0.25, "sparse_freq": 5}}
     t_test = Trajectory(1, fmodel, parameters)
     t_test.advance(0.012)
     assert isclose(t_test.score_max(), 0.12, abs_tol=1e-9)
@@ -175,68 +169,64 @@ def test_sparseSimpleTraj():
     assert t_test.is_converged() is True
     assert isclose(t_test.get_last_state(), 0.024, abs_tol=1e-9)
 
-def test_storeAndRestartSparseSimpleTraj():
+
+def test_store_and_restart_sparse_simple_traj():
     """Test a sparse trajectory with simple model."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 0.04,
-                                  "step_size": 0.001,
-                                  "targetscore": 0.25,
-                                  "sparse_freq": 5}}
+    parameters = {"trajectory": {"end_time": 0.04, "step_size": 0.001, "targetscore": 0.25, "sparse_freq": 5}}
     t_test = Trajectory(1, fmodel, parameters)
     t_test.advance(0.013)
     assert isclose(t_test.score_max(), 0.13, abs_tol=1e-9)
     assert t_test.is_converged() is False
-    chkFile = Path("./test.xml")
-    t_test.store(chkFile)
-    assert chkFile.exists() is True
-    rst_test = Trajectory.restore_from_checkfile(chkFile, fmodel, parameters)
+    chkfile = Path("./test.xml")
+    t_test.store(chkfile)
+    assert chkfile.exists() is True
+    rst_test = Trajectory.restore_from_checkfile(chkfile, fmodel, parameters)
     rst_test.advance()
     assert rst_test.is_converged() is True
-    chkFile.unlink()
+    chkfile.unlink()
 
-def test_scoreMovingAverage():
+
+def test_score_moving_average():
     """Test using a moving average on a score array."""
     fmodel = SimpleFModel
-    parameters = {"trajectory" : {"end_time": 0.9,
-                                  "step_size": 0.0001,
-                                  "targetscore": 0.95}}
+    parameters = {"trajectory": {"end_time": 0.9, "step_size": 0.0001, "targetscore": 0.95}}
     t_test = Trajectory(1, fmodel, parameters)
     t_test.advance()
     score = t_test.get_score_array()
     avg_score = moving_avg(score, 10)
-    assert isclose(avg_score[0],0.0045,abs_tol=1e-9)
+    assert isclose(avg_score[0], 0.0045, abs_tol=1e-9)
 
-def test_sparseDWTrajWithRestore():
+
+def test_sparse_dw_traj_with_restore():
     """Test restore a sparse trajectory with DW model."""
     fmodel = DoubleWellModel
-    parameters = {"tams" : {"deterministic" : True},
-                  "trajectory" : {"end_time": 15.0,
-                                  "step_size": 0.01,
-                                  "targetscore": 0.95,
-                                  "sparse_freq": 10},
-                  "model": {"noise_amplitude" : 0.8},
-                  }
+    parameters = {
+        "tams": {"deterministic": True},
+        "trajectory": {"end_time": 15.0, "step_size": 0.01, "targetscore": 0.95, "sparse_freq": 10},
+        "model": {"noise_amplitude": 0.8},
+    }
     t_test = Trajectory(1, fmodel, parameters)
     t_test.advance(4.07)
-    chkFile = Path("./test.xml")
-    t_test.store(chkFile)
-    assert isclose(t_test.score_max(), 0.49989802245025605, abs_tol=1e-9)
-    assert t_test.is_converged() == False
-    rst_test = Trajectory.restore_from_checkfile(chkFile, fmodel, parameters, frozen=False)
+    chkfile = Path("./test.xml")
+    t_test.store(chkfile)
+    assert isclose(t_test.score_max(), 0.5383998247480907, abs_tol=1e-9)
+    assert not t_test.is_converged()
+    rst_test = Trajectory.restore_from_checkfile(chkfile, fmodel, parameters, frozen=False)
     rst_test.advance()
     assert rst_test.score_max() > 0.95
-    assert rst_test.is_converged() == True
+    assert rst_test.is_converged()
+    chkfile.unlink(missing_ok=True)
 
-def test_sparseDWTrajWithBranching():
+
+def test_sparse_dw_traj_with_branching():
     """Test branching a sparse trajectory with simple model."""
     fmodel = DoubleWellModel
-    parameters = {"tams" : {"deterministic" : True},
-                  "trajectory" : {"end_time": 2.0,
-                                  "step_size": 0.01,
-                                  "targetscore": 0.95,
-                                  "sparse_freq": 10},
-                  "model": {"noise_amplitude" : 0.3},
-                  }
+    parameters = {
+        "tams": {"deterministic": True},
+        "trajectory": {"end_time": 2.0, "step_size": 0.01, "targetscore": 0.95, "sparse_freq": 10},
+        "model": {"noise_amplitude": 0.3},
+    }
     t_test = [Trajectory(1, fmodel, parameters), Trajectory(2, fmodel, parameters)]
     t_test[0].advance()
     t_test[1].advance()
