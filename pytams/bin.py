f4fc49185b037ec6b226cd81b8655d89c2ce07cb
Lucas Esclapez <lucas.esclapez@gmail.com>
2025-11-26 16:58:20 +0100

    Add tests on bin functions.

--- pytams/bin.py
+++ pytams/bin.py
@@ -7,19 +7,11 @@ from pytams.fmodel import ForwardModelBaseClass
 from pytams.utils import generate_subclass
 
 
-def tams_alive() -> None:
-    """Check pyTAMS."""
-    try:
-        print(f"== pyTAMS v{version('pytams')} :: a rare-event finder tool ==")  # noqa: T201
-    except PackageNotFoundError:
-        print("Package version not found")  # noqa: T201
+def parse_cl_args(a_args: list[str] | None = None) -> argparse.Namespace:
+    """Parse provided list or default CL argv.
 
-
-def tams_template_model() -> None:
-    """Copy a templated forward model file.
-
-    A helper function to help getting started from scratch
-    on a new model.
+    Args:
+        a_args: optional list of options
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -28,7 +20,26 @@ def tams_template_model() -> None:
         help="New mode class name",
         default="MyNewClass",
     )
+    return parser.parse_args() if a_args is None else parser.parse_args(a_args)
 
-    model_name = vars(parser.parse_args())["name"]
+
+def tams_alive() -> None:
+    """Check pyTAMS."""
+    try:
+        print(f"== pyTAMS v{version('pytams')} :: a rare-event finder tool ==")  # noqa: T201
+    except PackageNotFoundError:
+        print("Package version not found")  # noqa: T201
+
+
+def tams_template_model(a_args: list[str] | None = None) -> None:
+    """Copy a templated forward model file.
+
+    A helper function to help getting started from scratch
+    on a new model.
+
+    Args:
+        a_args: optional list of options
+    """
+    model_name = vars(parse_cl_args(a_args=a_args))["name"]
     out_file = f"{model_name}.py"
     generate_subclass(ForwardModelBaseClass, model_name, out_file)
