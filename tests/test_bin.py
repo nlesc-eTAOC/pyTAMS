0613ef6d6d6912e009c9926a1dfc45c7a3575122
Lucas Esclapez <lucas.esclapez@gmail.com>
2026-02-27 16:34:25 +0100

    Add tams_run tests and update for fmodel file changes.

--- tests/test_bin.py
+++ tests/test_bin.py
@@ -1,8 +1,10 @@
 """Tests for the pytams.bin functions."""
 from pathlib import Path
 import pytest
+import toml
 from pytams.bin import tams_alive
 from pytams.bin import tams_template_model
+from pytams.bin import tams_run
 
 
 def test_tams_alive(capsys: pytest.CaptureFixture[str]):
@@ -22,3 +24,16 @@ def test_tams_template_model_with_name():
     assert Path("./MyCustomClass.py").exists()
     Path("./MyCustomClass.py").unlink(missing_ok=True)
 
+def test_tams_run():
+    """Test TAMS run."""
+    params_dict = {
+        "tams": {"ntrajectories": 20, "nsplititer": 20, "walltime": 20.0, "deterministic": True},
+        "runner": {"type": "asyncio", "nworker_init": 1, "nworker_iter": 1},
+        "trajectory": {"end_time": 6.0, "step_size": 0.01, "targetscore": 0.6},
+        "model": {"slow_factor": 0.00000001, "noise_amplitude": 0.6},
+    }
+    with Path("input.toml").open("w") as f:
+        toml.dump(params_dict, f)
+    tams_run(a_args=["-m", "./tests/dwmodel.py",
+                     "-i", "input.toml"])
+    Path("input.toml").unlink(missing_ok=True)
