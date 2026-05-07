ee8657c04dfaca215d4098a14aa65fc05baa98a2
Lucas Esclapez <lucas.esclapez@gmail.com>
2026-05-07 13:31:28 +0200

    Fix __all__ order in runner.

--- pytams/runner/__init__.py
+++ pytams/runner/__init__.py
@@ -3,4 +3,4 @@ from .taskrunner import make_runner
 from .worker import ms_worker
 from .worker import pool_worker
 
-__all__ = ["make_runner", "ms_worker", "pool_worker", "RunnerConfig"]
+__all__ = ["RunnerConfig", "make_runner", "ms_worker", "pool_worker"]
