14baef97b835b4234b3f110a112cbe753b64c9d5
Lucas Esclapez <lucas.esclapez@gmail.com>
2026-05-08 15:42:36 +0200

    Update bichannel.

--- examples/BiChannel2D/sample_bichannel.py
+++ examples/BiChannel2D/sample_bichannel.py
@@ -1,7 +1,7 @@
 import numpy as np
 from bichannel2d import BiChannel2D
 from bichannel2d import plot_in_landscape
-from pytams.tams import TAMS
+from pytams.sampler import build_sampler
 
 if __name__ == "__main__":
     # For convenience
@@ -11,18 +11,19 @@ if __name__ == "__main__":
     plot_ensemble = False
 
     # Number of consecutive TAMS runs
-    K = 10
+    K = 1
 
     probabilities = np.zeros(K)
 
     # Run the model several times
     for i in range(K):
-        # Initialize the algorithm object
-        tams = TAMS(fmodel_t=fmodel)
+        # Initialize the sampler
+        sampler = build_sampler(fmodel_t=fmodel)
 
-        # Run TAMS and report
+        # Sample and report
         try:
-            probability = tams.compute_probability()
+            sampler.run()
+            probability = sampler.database.get_event_probability()
         except RuntimeError as e:
             print(e)  # noqa: T201
             continue
@@ -32,6 +33,6 @@ if __name__ == "__main__":
         print(f"[{i}] : {probability}")  # noqa: T201
 
         if plot_ensemble:
-            plot_in_landscape(fmodel, tams.get_database(), i)
+            plot_in_landscape(fmodel, sampler.database(), i)
 
     print(f"Averaged transition P_K: {probabilities.mean()}, RE: {np.sqrt(probabilities.var()) / probabilities.mean()}")  # noqa : T201
