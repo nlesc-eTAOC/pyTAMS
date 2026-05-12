d46270d9cb6c7e091b49b6db1e7b0a3046849ac4
Lucas Esclapez <lucas.esclapez@gmail.com>
2026-05-12 11:51:18 +0200

    Add a Protocol for termination criterion.

---
+++ pytams/strategies/base/termination.py
@@ -0,0 +1,80 @@
+"""Defines the interface and simple implementations of termination criteria."""
+
+from typing import Protocol
+from typing import TypeVar
+from pytams.core import ForwardModelBaseClass
+from pytams.trajectory import Trajectory
+
+T_Noise = TypeVar("T_Noise")
+T_State = TypeVar("T_State")
+
+
+class TerminationCriterion(Protocol):
+    """Termination criterion interface."""
+
+    def should_terminate(
+        self,
+        model: ForwardModelBaseClass[T_Noise, T_State],
+        trajectory: Trajectory,
+    ) -> bool:
+        """Check if the trajectory should terminate.
+
+        Args:
+            model: the forward model
+            trajectory: the trajectory
+
+        Returns:
+            True if the trajectory should terminate
+        """
+        raise NotImplementedError
+
+
+class TimeTerminationCriterion(TerminationCriterion):
+    """Termination criterion based on time.
+
+    Will trigger termination if the current time is greater than or equal
+    to the end time.
+    """
+
+    def __init__(self, end_time: float) -> None:
+        """Initialize the termination criterion.
+
+        Args:
+            end_time: the end time
+        """
+        self._end_time = end_time
+
+    def should_terminate(
+        self,
+        model: ForwardModelBaseClass[T_Noise, T_State],
+        trajectory: Trajectory,
+    ) -> bool:
+        """Check if the trajectory should terminate."""
+        _ = model
+        return trajectory.current_time() >= self._end_time
+
+
+class LowScoreTerminationCriterion(TerminationCriterion):
+    """Termination criterion based on score.
+
+    Will trigger termination if the current score is less than or equal
+    to a threshold.
+
+    """
+
+    def __init__(self, score_threshold: float) -> None:
+        """Initialize the termination criterion.
+
+        Args:
+            score_threshold: the score threshold
+        """
+        self._score_threshold = score_threshold
+
+    def should_terminate(
+        self,
+        model: ForwardModelBaseClass[T_Noise, T_State],
+        trajectory: Trajectory,
+    ) -> bool:
+        """Check if the trajectory should terminate."""
+        _ = trajectory
+        return model.score() <= self._score_threshold
